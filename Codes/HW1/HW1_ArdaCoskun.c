0e973bccb547f444cb00e7094cde48b4f5a435f9
Kullanici Adiniz <inteera@protonmail.com>
2024-09-25 17:05:28 +0300

    Added FirstTest and moved test files to it

--- Codes/HW1/HW1_ArdaCoskun.c
+++ Codes/HW1/HW1_ArdaCoskun.c
@@ -2,17 +2,16 @@
 
 int main()
 {
-    char courseSection[20] = "2";
-    char studentId[20] = "240508008";
-    char name[20] = "Arda Coskun";
+  char courseSection[20] = "2";
+  char studentId[20] = "240508008";
+  char name[20] = "Arda Coskun";
 
-    //Sorry i don't know my lab assistant name
-    char labAssistantName[25] = "Prof. Muhammet Abdelati";
+  //Sorry i don't know my lab assistant name
+  char labAssistantName[25] = "Prof. Muhammet Abdelati";
 
-    printf("%s, %s\n", studentId, name);
-    printf("%s\n", courseSection);
-    printf("%s", labAssistantName);
+  printf("%s, %s\n", studentId, name);
+  printf("%s\n", courseSection);
+  printf("%s", labAssistantName);
 
-
-    return 0;
+  return 0;
 }
