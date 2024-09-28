c9db051793c8cb1c310b01f9f9592db8eb2981f9
Kullanici Adiniz <inteera@protonmail.com>
2024-09-28 14:28:22 +0300

    Homework 2, v2

--- Codes/HW2/HW2_ArdaCoskun.c
+++ Codes/HW2/HW2_ArdaCoskun.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+#define TOP_SPACE    "\n\n\n\n\n"
+#define BOTTOM_SPACE "\n\n\n\n\n"
+#define OFFSET       "          "
+
 int main()
 {
-  printf("IIIIIIIII\n");
+  printf(TOP_SPACE);
+  printf(OFFSET "IIIIIIIII\n");
   for(short i = 0; i <= 8; i++)
   {
-    printf("   III\n");
+    printf(OFFSET "   III\n");
   }
-  printf("IIIIIIIII\n");
+  printf(OFFSET "IIIIIIIII");
+  printf(BOTTOM_SPACE);
 
   return 0;
 }
