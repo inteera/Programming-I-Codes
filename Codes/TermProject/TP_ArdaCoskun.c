b6ac6dd93527fe479d65c0b0be95f4fe0198f0bb
Arda Coskun <inteera@protonmail.com>
2024-12-09 15:07:13 +0300

    TP v2 #1

--- Codes/TermProject/TP_ArdaCoskun.c
+++ Codes/TermProject/TP_ArdaCoskun.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 
+struct Student{
+    char name[20];
+    char number[10];
+    char nationalID[12];
+    char bookletType;
+    char answers[51];
+    int grade;
+} typedef Student;
+
 int main(){
 
     FILE *answerKeys = fopen("AnswerKeys.txt", "r");
@@ -8,13 +17,11 @@ int main(){
     FILE *grades = fopen("Grades.txt", "w");
     FILE *statistics = fopen("Statistics.txt", "w");
 
+    int stdno = 384;
     char answers[2][52];
-    char card[143];
-    short int order = 1;
-    short int mt;
-    short int temp;
-    short int key;
-    short int error = 0;
+    Student students[stdno];
+    int temp;
+    int error = 0;
     
     fgets(answers[0], 53, answerKeys);
     fgets(answers[1], 53, answerKeys);
@@ -22,9 +29,19 @@ int main(){
     //Header
     fprintf(grades, "sn. ID        Name                 MT");
     fprintf(grades, "\n");
-    while(fgets(card, 143, cards) != NULL){
-
+    //143
+    for(int i = 0; i < stdno; i++){
+        fgets(students[i].name, 20, cards);
+        fgets(students[i].nationalID, 13, cards);
+        fgets(students[i].number, 10, cards);
+        fgets(&students[i].bookletType, 2, cards);
+        fgets(students[i].answers, 51, cards);
+        printf("%s  %s  %s  %c  %s", students[i].name, students[i].nationalID, students[i].number, students[i].bookletType, students[i].answers);
+        fprintf(grades, "%03d ", i + 1);
+    }
+        /*
         //Is the name writable
+        fgets(card, 143, cards)
         for (int i = 0; i < 20; i++) {
             if (!isprint(card[i])) {
                 error = 1;
@@ -69,7 +86,8 @@ int main(){
             fgets(card, 143, cards);
         }
         error = 0;
-    }
+        */
+        
 
     return 0;
 } 
