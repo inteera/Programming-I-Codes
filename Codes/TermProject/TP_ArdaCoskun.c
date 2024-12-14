dd8a53ca1969dc9686df32b4bf42bff91543026a
Arda Coskun <inteera@protonmail.com>
2024-12-14 16:07:44 +0300

    TP v2 #4
    TODO:
    1- Make functions for every operation
    2- Make statistics for questions
    3- Write to statistics file

--- Codes/TermProject/TP_ArdaCoskun.c
+++ Codes/TermProject/TP_ArdaCoskun.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 struct Student{
-    char name[20];
+    unsigned char name[20];
     char number[10];
     char nationalID[12];
     char bookletType;
@@ -19,9 +19,14 @@ int main(){
 
     int stdno = 384;
     char answers[2][52];
+    int answerStatistics[2][100] = {0};
+    int countOfBooklets[2] = {0};
+    int minGrade;
+    int maxGrade;
+    int avgGrade = 0;
     Student students[stdno];
     int key = 0;
-    int temp;
+    int temp = 0;
     char tempc[143];
     int error = 0;
     
@@ -33,11 +38,30 @@ int main(){
 
     for(int i = 0; i < stdno; i++){
         fgets(students[i].name, 20, cards);
-        fgets(students[i].nationalID, 13, cards);
+        for(int j = 0; j < 20; j++){
+            if(students[i].name[j] > 128){
+                 temp = 1;
+                 /*switch (students[i].name[j]){
+                    case 128:
+                        students[i].name[j] = 67;
+                    case 129:
+                        students[i].name[j] = 117;
+                    case 135:
+                        students[i].name[j] = 99;
+                    case 153:
+                        students[i].name[j] = 79;
+                    case 154:
+                        students[i].name[j] = 85;
+                    default: 
+                        break;
+                 }*/
+            }
+        }
+        fgets(students[i].nationalID, 13 + temp, cards);
         fgets(students[i].number, 10, cards);
         fgets(&students[i].bookletType, 2, cards);
         fgets(students[i].answers, 103, cards);
-
+        temp = 0;
         for (int j = 0; j < 20; j++) {
             if (!isprint(students[i].name[i])) {
                 error = 1;
@@ -46,15 +70,32 @@ int main(){
 
         students[i].grade = 0;
         key = students[i].bookletType == 'A' ? 0 : 1;
-        for(int temp = 0; temp < 50; temp++){
-            if(answers[key][temp + 1] == students[i].answers[temp]) students[i].grade += 2;
+        countOfBooklets[key] += 1;
+        for (int j = 0; answers[key][j + 1] != '\0'; j++) {
+            if (answers[key][j + 1] == students[i].answers[j]) {
+                students[i].grade += 2;
+                answerStatistics[key][j] += 1;
+            }
         }
-
     }
+
+    minGrade = students[0].grade;
+    maxGrade = students[0].grade;
+    int sum = 0;
+    for(int i = 0; i < stdno; i++){
+        if(students[i].grade < minGrade) minGrade = students[i].grade;
+        if(students[i].grade > maxGrade) maxGrade = students[i].grade;
+        sum += students[i].grade;
+    }
+    avgGrade = sum / stdno;
     for(int i = 0; i < stdno; i++){
         fprintf(grades, "%03d %s %-20s %3d\n", i + 1, students[i].number, students[i].name, students[i].grade);
         printf("%03d %s %-20s %3d\n", i + 1, students[i].number, students[i].name, students[i].grade);
     }
+    /*for(int i = 0; i < 100; i++){
+        printf("%d. - %d\n", i + 1, answerStatistics[i]);
+    }*/
+    printf("%d, %d, %d", minGrade, maxGrade, avgGrade);
 
     return 0;
 } 
