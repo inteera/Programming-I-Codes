a452d78597ffa055d30d9ae923bbc2c6d824c4cf
Arda Coskun <inteera@protonmail.com>
2024-12-15 15:40:02 +0300

    TP v2 Finished

--- Codes/TermProject/TP_ArdaCoskun.c
+++ Codes/TermProject/TP_ArdaCoskun.c
@@ -1,61 +1,46 @@
 #include <stdio.h>
 #include <ctype.h>
 
-struct Student{
+#define STDNO 384
+
+typedef struct {
     unsigned char name[20];
     char number[10];
     char nationalID[12];
     char bookletType;
     char answers[101];
     int grade;
-} typedef Student;
+} Student;
 
-int main(){
+void readAnswerKeys(FILE *file, char answers[2][52]) {
+    fgets(answers[0], 53, file);
+    fgets(answers[1], 53, file);
+}
 
-    FILE *answerKeys = fopen("AnswerKeys.txt", "r");
-    FILE *cards = fopen("Cards.txt", "r");
-    FILE *grades = fopen("Grades.txt", "w");
-    FILE *statistics = fopen("Statistics.txt", "w");
-
-    int stdno = 384;
-    char answers[2][52];
-    int answerStatistics[2][100] = {0};
-    int countOfBooklets[2] = {0};
-    int minGrade;
-    int maxGrade;
-    int avgGrade = 0;
-    Student students[stdno];
-    int key = 0;
+void readStudentData(FILE *file, Student *students, int *countOfBooklets) {
     int temp = 0;
-    int error = 0;
-    
-    fgets(answers[0], 53, answerKeys);
-    fgets(answers[1], 53, answerKeys);
-
-    fprintf(grades, "sn. ID        Name                 MT");
-    fprintf(grades, "\n");
-
-    for(int i = 0; i < stdno; i++){
-        fgets(students[i].name, 20, cards);
-        for(int j = 0; j < 20; j++){
-            if(students[i].name[j] > 128){
+    for (int i = 0; i < STDNO; i++) {
+        fgets(students[i].name, 20, file);
+        for (int j = 0; j < 20; j++) {
+            if (students[i].name[j] > 128) {
                 temp = 1;
             }
         }
-        fgets(students[i].nationalID, 13 + temp, cards);
-        fgets(students[i].number, 10, cards);
-        fgets(&students[i].bookletType, 2, cards);
-        fgets(students[i].answers, 103, cards);
+        fgets(students[i].nationalID, 13 + temp, file);
+        fgets(students[i].number, 10, file);
+        fgets(&students[i].bookletType, 2, file);
+        fgets(students[i].answers, 103, file);
         temp = 0;
-        for (int j = 0; j < 20; j++) {
-            if (!isprint(students[i].name[i])) {
-                error = 1;
-            }
-        }
-
-        students[i].grade = 0;
-        key = students[i].bookletType == 'A' ? 0 : 1;
+        
+        int key = students[i].bookletType == 'A' ? 0 : 1;
         countOfBooklets[key] += 1;
+    }
+}
+
+void calculateGrades(Student *students, char answers[2][52], int answerStatistics[2][100]) {
+    for (int i = 0; i < STDNO; i++) {
+        int key = students[i].bookletType == 'A' ? 0 : 1;
+        students[i].grade = 0;
         for (int j = 0; answers[key][j + 1] != '\0'; j++) {
             if (answers[key][j + 1] == students[i].answers[j]) {
                 students[i].grade += 2;
@@ -63,37 +48,70 @@ int main(){
             }
         }
     }
+}
 
-    minGrade = students[0].grade;
-    maxGrade = students[0].grade;
+void calculateStatistics(Student *students, int *minGrade, int *maxGrade, int *avgGrade) {
+    *minGrade = students[0].grade;
+    *maxGrade = students[0].grade;
     int sum = 0;
-    for(int i = 0; i < stdno; i++){
-        if(students[i].grade < minGrade) minGrade = students[i].grade;
-        if(students[i].grade > maxGrade) maxGrade = students[i].grade;
+
+    for (int i = 0; i < STDNO; i++) {
+        if (students[i].grade < *minGrade) *minGrade = students[i].grade;
+        if (students[i].grade > *maxGrade) *maxGrade = students[i].grade;
         sum += students[i].grade;
     }
-    avgGrade = sum / stdno;
-    for(int i = 0; i < stdno; i++){
-        fprintf(grades, "%03d %s %-20s %3d\n", i + 1, students[i].number, students[i].name, students[i].grade);
-    }
-    
-    fprintf(grades, "              Average               %d\n", avgGrade);
-    fprintf(grades, "              Maximum               %d\n", maxGrade);
-    fprintf(grades, "              Minimum               %d\n", minGrade);
+    *avgGrade = sum / STDNO;
+}
 
-    fprintf(statistics, "Count of A booklets   = %d\n", countOfBooklets[0]);
-    fprintf(statistics, "Count of B booklets   = %d\n", countOfBooklets[1]);
-    fprintf(statistics, "Count of all booklets = %d\n\n", countOfBooklets[0] + countOfBooklets[1]);
-    fprintf(statistics, "Using Booklet A numbering:\n");
-    fprintf(statistics, "Q    NoC\n");
-    for(int i = 0; i < 50; i++){
-        fprintf(statistics, "%03d  %3d\n", i+1, (i+10 <= 50) ? answerStatistics[0][i] + answerStatistics[1][i+10] : answerStatistics[0][i] + answerStatistics[1][i-40]);
+void writeGrades(FILE *gradesFile, Student *students, int minGrade, int maxGrade, int avgGrade) {
+    fprintf(gradesFile, "sn. ID        Name                 MT\n");
+    for (int i = 0; i < STDNO; i++) {
+        fprintf(gradesFile, "%03d %s %-20s %3d\n", i + 1, students[i].number, students[i].name, students[i].grade);
     }
+    fprintf(gradesFile, "              Average               %d\n", avgGrade);
+    fprintf(gradesFile, "              Maximum               %d\n", maxGrade);
+    fprintf(gradesFile, "              Minimum               %d\n", minGrade);
+}
+
+void writeStatistics(FILE *statisticsFile, int countOfBooklets[2], int answerStatistics[2][100]) {
+    fprintf(statisticsFile, "Count of A booklets   = %d\n", countOfBooklets[0]);
+    fprintf(statisticsFile, "Count of B booklets   = %d\n", countOfBooklets[1]);
+    fprintf(statisticsFile, "Count of all booklets = %d\n\n", countOfBooklets[0] + countOfBooklets[1]);
+    fprintf(statisticsFile, "Using Booklet A numbering:\n");
+    fprintf(statisticsFile, "Q    NoC\n");
+    for (int i = 0; i < 50; i++) {
+        fprintf(statisticsFile, "%03d  %3d\n", i + 1, (i + 10 <= 50) ? answerStatistics[0][i] + answerStatistics[1][i + 10] : answerStatistics[0][i] + answerStatistics[1][i - 40]);
+    }
+}
+
+int main() {
+    FILE *answerKeys = fopen("AnswerKeys.txt", "r");
+    FILE *cards = fopen("Cards.txt", "r");
+    FILE *grades = fopen("Grades.txt", "w");
+    FILE *statistics = fopen("Statistics.txt", "w");
+
+    if (!answerKeys || !cards || !grades || !statistics) {
+        printf("Cannot open one or more of the files.");
+        return 1;
+    }
+
+    char answers[2][52];
+    int answerStatistics[2][100] = {0};
+    int countOfBooklets[2] = {0};
+    int minGrade, maxGrade, avgGrade;
+    Student students[STDNO];
+
+    readAnswerKeys(answerKeys, answers);
+    readStudentData(cards, students, countOfBooklets);
+    calculateGrades(students, answers, answerStatistics);
+    calculateStatistics(students, &minGrade, &maxGrade, &avgGrade);
+    writeGrades(grades, students, minGrade, maxGrade, avgGrade);
+    writeStatistics(statistics, countOfBooklets, answerStatistics);
 
     fclose(answerKeys);
     fclose(cards);
     fclose(grades);
     fclose(statistics);
-    
+
     return 0;
-} 
+}
---
+++ Codes/TermProject/a.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define stdno 384
+
+struct Student{
+    unsigned char name[20];
+    char number[10];
+    char nationalID[12];
+    char bookletType;
+    char answers[101];
+    int grade;
+} typedef Student;
+
+int main(){
+
+    FILE *answerKeys = fopen("AnswerKeys.txt", "r");
+    FILE *cards = fopen("Cards.txt", "r");
+    FILE *grades = fopen("Grades.txt", "w");
+    FILE *statistics = fopen("Statistics.txt", "w");
+
+    char answers[2][52];
+    int answerStatistics[2][100] = {0};
+    int countOfBooklets[2] = {0};
+    int minGrade, maxGrade, avgGrade;
+    Student students[stdno];
+    int key = 0;
+    int temp = 0;
+    
+    fgets(answers[0], 53, answerKeys);
+    fgets(answers[1], 53, answerKeys);
+
+    fprintf(grades, "sn. ID        Name                 MT");
+    fprintf(grades, "\n");
+
+    for(int i = 0; i < stdno; i++){
+        fgets(students[i].name, 20, cards);
+        for(int j = 0; j < 20; j++){
+            if(students[i].name[j] > 128){
+                temp = 1;
+            }
+        }
+        fgets(students[i].nationalID, 13 + temp, cards);
+        fgets(students[i].number, 10, cards);
+        fgets(&students[i].bookletType, 2, cards);
+        fgets(students[i].answers, 103, cards);
+        temp = 0;
+
+        students[i].grade = 0;
+        key = students[i].bookletType == 'A' ? 0 : 1;
+        countOfBooklets[key] += 1;
+        for (int j = 0; answers[key][j + 1] != '\0'; j++) {
+            if (answers[key][j + 1] == students[i].answers[j]) {
+                students[i].grade += 2;
+                answerStatistics[key][j] += 1;
+            }
+        }
+    }
+
+    minGrade = students[0].grade;
+    maxGrade = students[0].grade;
+    int sum = 0;
+    for(int i = 0; i < stdno; i++){
+        if(students[i].grade < minGrade) minGrade = students[i].grade;
+        if(students[i].grade > maxGrade) maxGrade = students[i].grade;
+        sum += students[i].grade;
+    }
+    avgGrade = sum / stdno;
+    for(int i = 0; i < stdno; i++){
+        fprintf(grades, "%03d %s %-20s %3d\n", i + 1, students[i].number, students[i].name, students[i].grade);
+    }
+    
+    fprintf(grades, "              Average               %d\n", avgGrade);
+    fprintf(grades, "              Maximum               %d\n", maxGrade);
+    fprintf(grades, "              Minimum               %d\n", minGrade);
+
+    fprintf(statistics, "Count of A booklets   = %d\n", countOfBooklets[0]);
+    fprintf(statistics, "Count of B booklets   = %d\n", countOfBooklets[1]);
+    fprintf(statistics, "Count of all booklets = %d\n\n", countOfBooklets[0] + countOfBooklets[1]);
+    fprintf(statistics, "Using Booklet A numbering:\n");
+    fprintf(statistics, "Q    NoC\n");
+    for(int i = 0; i < 50; i++){
+        fprintf(statistics, "%03d  %3d\n", i+1, (i+10 <= 50) ? answerStatistics[0][i] + answerStatistics[1][i+10] : answerStatistics[0][i] + answerStatistics[1][i-40]);
+    }
+
+    fclose(answerKeys);
+    fclose(cards);
+    fclose(grades);
+    fclose(statistics);
+
+    return 0;
+}
